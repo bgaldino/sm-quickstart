9667b7cb7fc760aeecb813fc8430ea216f3c792a
Brian Galdino <bgaldino@salesforce.com>
2022-06-07 14:13:21 -0700

    Optimize org settings deployment

--- scripts/set-org-settings.sh
+++ scripts/set-org-settings.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
-apiVersion="55.0"
+apiversion="55.0"
+baseDir="sm/sm-base/main/default/settings"
+
+declare -a settingsArray=(
+  "$baseDir/Order.settings-meta.xml"
+  "$baseDir/Quote.settings-meta.xml"
+  "$baseDir/SubscriptionManagement.settings-meta.xml"
+  "$baseDir/InvLatePymntRiskCalc.settings-meta.xml"
+  "$baseDir/PaymentsManagementEnabled.settings-meta.xml"
+  "$baseDir/Communities.settings-meta.xml"
+)
 
 function echo_attention() {
   local green='\033[0;32m'
@@ -7,10 +17,32 @@ function echo_attention() {
   echo "${green}$1${no_color}"
 }
 
-baseDir="sm/sm-base/main"
-sfdx force:source:deploy -p $baseDir/default/settings/Order.settings-meta.xml --apiversion=$apiVersion
-sfdx force:source:deploy -p $baseDir/default/settings/Quote.settings-meta.xml --apiversion=$apiVersion
-sfdx force:source:deploy -p $baseDir/default/settings/SubscriptionManagement.settings-meta.xml --apiversion=$apiVersion
-sfdx force:source:deploy -p $baseDir/default/settings/InvLatePymntRiskCalc.settings-meta.xml --apiversion=$apiVersion
-sfdx force:source:deploy -p $baseDir/default/settings/PaymentsManagementEnabled.settings-meta.xml --apiversion=$apiVersion
-sfdx force:source:deploy -p $baseDir/default/settings/Communities.settings-meta.xml --apiversion=$apiVersion
+function echo_blue() {
+  local blue='\033[0;34m'
+  local no_color='\033[0m'
+  echo "${blue}$1${no_color}"
+}
+
+function error_and_exit() {
+  echo "$1"
+  exit 1
+}
+
+function deploy() {
+  sfdx force:source:deploy -p "$1" -g --apiversion=$apiversion
+}
+
+function deploy_settings() {
+  local ps=("$@")
+  local delim=""
+  local joined=""
+  for i in "${ps[@]}"; do
+    joined="$joined$delim$i"
+    delim=","
+  done
+  echo_attention "Deploying default org settings"
+  #echo_blue $joined
+  deploy $joined
+}
+
+deploy_settings "${settingsArray[@]}"
