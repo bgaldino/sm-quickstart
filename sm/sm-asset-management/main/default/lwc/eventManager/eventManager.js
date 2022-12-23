1da464b65de8450196f9d0b43fb6213417b4bbec
Brian Galdino <bgaldino@mac.com>
2022-12-22 22:51:30 -0800

    Add Amend Event

--- sm/sm-asset-management/main/default/lwc/eventManager/eventManager.js
+++ sm/sm-asset-management/main/default/lwc/eventManager/eventManager.js
@@ -10,12 +10,14 @@ export default class EventManager extends LightningElement {
     renewSubscription = {};
      @api cancelChannel = '/event/AssetCancelInitiatedEvent';
      @api renewChannel = '/event/AssetRenewInitiatedEvent';
+     @api amendChannel = '/event/AssetAmendInitiatedEvent';
 
     // Initializes the component
     connectedCallback() {
         // Register listener
         this.handleRenewSubscribe();
         this.handleCancelSubscribe();
+        this.handleAmendSubscribe();
     }
 
     disconnectedCallback() {
@@ -85,11 +87,44 @@ export default class EventManager extends LightningElement {
             this.renewSubscription = response;
         });
     }
+
+    handleAmendSubscribe = () => {
+        // Callback invoked whenever a new event message is received
+        const thisReference = this;
+        const messageCallback = function(response) {
+            let obj = JSON.parse(JSON.stringify(response));
+            
+            if(currentUserId === obj.data.payload.CreatedById){ //dispatch the users events instead of all events
+
+                let message = 'Order created: ' + obj.data.payload.AmendmentRecordId;
+                let variant = 'success'
+                if (obj.data.payload.AssetAmendErrorDetailEvents != null) {
+                    message = 'Couldn’t amend the asset: ' + obj.data.payload.AssetAmendErrorDetailEvents[0].ErrorMessage;
+                    variant = 'error'
+                }
+                const evt = new ShowToastEvent({
+                    message: message,
+                    variant: variant,
+                    mode : "sticky"
+                });
+
+                thisReference.dispatchEvent(evt);
+            }
+            // Response contains the payload of the new message received
+        };
+
+        subscribe(this.renewChannel, -1, messageCallback).then(response => {
+            // Response contains the subscription information on renew call
+            this.renewSubscription = response;
+        });
+    }
     
     unsubscribeEvents() {
         unsubscribe(this.renewSubscription);
         unsubscribe(this.cancelSubscription);
+        unsubscribe(this.amendSubscription)
         this.cancelSubscription = null;
         this.renewSubscription = null;
+        this.amendSubscription = null;
     }
 }
