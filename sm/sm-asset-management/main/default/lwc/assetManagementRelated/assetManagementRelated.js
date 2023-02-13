385bafea60683f6bbb84a01835f4d2758d6878eb
Brian Galdino <bgaldino@mac.com>
2023-02-13 12:08:23 -0800

    merge changes from labs

--- sm/sm-asset-management/main/default/lwc/assetManagementRelated/assetManagementRelated.js
+++ sm/sm-asset-management/main/default/lwc/assetManagementRelated/assetManagementRelated.js
@@ -1,153 +1,257 @@
-import { LightningElement, wire, track, api } from "lwc";
-import getAssets from "@salesforce/apex/AssetManagementController.getAssetsByAccount";
-import renewAssets from "@salesforce/apex/AssetManagementController.renewAssets";
-import cancelAssets from "@salesforce/apex/AssetManagementController.cancelAssets";
-import amendAssets from "@salesforce/apex/AssetManagementController.amendAssets";
+import { LightningElement, wire, track, api } from 'lwc';
+import getAssets from '@salesforce/apex/AssetManagementController.getAssetsByAccount'
+import renewAssets from '@salesforce/apex/AssetManagementController.renewAssets'
+import cancelAssets from '@salesforce/apex/AssetManagementController.cancelAssets'
+import amendAssets from '@salesforce/apex/AssetManagementController.amendAssets'
+import AssetManagementLabel from '@salesforce/label/c.AssetManagement';
 
 const columns = [
-  { label: "Asset Name", fieldName: "Name", type: "text" },
-  { label: "Current Quantity", fieldName: "CurrentQuantity", type: "number" },
-  {
-    label: "Lifecycle Start Date",
-    fieldName: "LifecycleStartDate",
-    type: "date",
-    typeAttributes: {
-      day: "2-digit",
-      month: "2-digit",
-      year: "numeric",
-      timeZone: "UTC"
-    }
-  },
-  {
-    label: "Lifecycle End Date",
-    fieldName: "LifecycleEndDate",
-    type: "date",
-    typeAttributes: {
-      day: "2-digit",
-      month: "2-digit",
-      year: "numeric",
-      timeZone: "UTC"
-    }
-  },
-  { label: "Renewal Term Unit", fieldName: "RenewalTermUnit", type: "text" },
-  { label: "Renewal Term", fieldName: "RenewalTerm", type: "Number" }
+    {
+        label: 'Asset Name', fieldName: 'recordURL', type: 'url',
+        typeAttributes: { label: { fieldName: 'name' }, target: '_blank' }
+    },
+    {
+        label: 'Lifecycle Start Date', fieldName: 'lifeCycleStartDate', type: 'date', typeAttributes: {
+            day: "2-digit",
+            month: "2-digit",
+            year: "numeric",
+            timeZone: "UTC"
+        }
+    },
+    {
+        label: 'Lifecycle End Date', fieldName: 'lifeCycleEndDate', type: 'date', typeAttributes: {
+            day: "2-digit",
+            month: "2-digit",
+            year: "numeric",
+            timeZone: "UTC"
+        }
+    },
+    { label: 'Renewal Term Unit', fieldName: 'renewalTermUnit', type: 'text' },
+    { label: 'Renewal Term', fieldName: 'renewalTerm', type: 'Number' },
 ];
 
+const columnsUpdated = [
+    {
+        label: 'Asset Name', fieldName: 'recordURL', type: 'url',
+        typeAttributes: { label: { fieldName: 'name' }, target: '_blank' }
+    },
+    {
+        label: 'Lifecycle Start Date', fieldName: 'lifeCycleStartDate', type: 'date', typeAttributes: {
+            day: "2-digit",
+            month: "2-digit",
+            year: "numeric",
+            timeZone: "UTC"
+        }
+    },
+    {
+        label: 'Lifecycle End Date', fieldName: 'lifeCycleEndDate', type: 'date', typeAttributes: {
+            day: "2-digit",
+            month: "2-digit",
+            year: "numeric",
+            timeZone: "UTC"
+        }
+    },
+    { label: 'Renewal Term Unit', fieldName: 'renewalTermUnit', type: 'text' },
+    { label: 'Renewal Term', fieldName: 'renewalTerm', type: 'Number' },
+    {
+        label: 'Status', fieldName: 'StatusURL', type: 'url',
+        typeAttributes: { label: { fieldName: 'Status' }, target: '_blank' }
+    },
+];
+
+
 export default class AssetManagement extends LightningElement {
-  isButtonsActivated = true;
-  isCancelDatePopup = false;
-  isAmendDatePopup = false;
-  @track assetList;
-  @track error;
-  @api recordId;
-  @track columns = columns;
-  selectedRows = [];
-  @track cancelledDate;
-  @track amendedDate;
-  @track amendQuantity;
-  @track isLoaded = false;
+    @track assetList;
+    @track error;
+    @track columns = columns;
+    @track cancelledDate
+    @track amendedDate;
+    @track isToggle = {isLoaded: false, isCancelDatePopup: false, isButtonsActivated: true, isAmendDatePopup: false }
+    @track selectedRows = [];
+    @track selectedRowsAPI = []
+    @track AssetManagementLabel = AssetManagementLabel
+    @api recordId
+    assetMap = new Map();
+    a_Record_URL;
+    asyncIdList = new Set();
+    quantity;
 
-  @wire(getAssets, { accountId: "$recordId" })
-  assets({ error, data }) {
-    if (data) {
-      this.assetList = data;
-    } else if (error) {
-      this.error = error;
-      this.assetList = undefined;
+    connectedCallback() {
+        this.a_Record_URL = window.location.origin;
     }
-  }
 
-  handleRowSelection = (event) => {
-    this.selectedRows = event.detail.selectedRows;
-    console.log(this.selectedRows);
-    this.isButtonsActivated = this.selectedRows.length > 0 ? false : true;
-  };
-
-  toggleCancelDatePopup = () => {
-    this.isCancelDatePopup = this.isCancelDatePopup === false ? true : false;
-  };
-
-  toggleAmendDatePopup = () => {
-    this.isAmendDatePopup = this.isAmendDatePopup === false ? true : false;
-  };
-
-  handleAction = (event) => {
-    let actionType = event.currentTarget.name;
-
-    this.isLoaded = true;
-    console.log("actionType", actionType);
-    if (actionType === "Renew") {
-      this.handleRenewAssets();
-    } else if (actionType === "Cancel") {
-      this.handleCancelAssets();
-    } else if (actionType === "Amend") {
-      this.handleAmendAssets();
+    @wire(getAssets, { accountId: '$recordId' })
+    assets({ error, data }) {
+        if (data) {
+            this.assetList = this.generateTree(data)
+        }
+        else if (error) {
+            this.error = error;
+            this.assetList = undefined;
+        }
     }
-    this.isLoaded = false;
-    this.isCancelDatePopup = false;
-    this.isAmendDatePopup = false;
-  };
 
-  handleRenewAssets = () => {
-    renewAssets({ assetList: this.selectedRows })
-      .then((data) => {
-        console.log(data);
-      })
-      .catch((error) => {
-        this.error = error;
-      });
-  };
+    generateTree(data) {
+        data.forEach(element => {
+            let tempConRec = Object.assign({}, element);
+            tempConRec._children = []
+            this.assetMap.set(tempConRec.assetId, tempConRec);
+        })
+        let assetList = Array.from(this.assetMap.values());
+        let r = [], h = assetList.reduce((a, c) => (a[c.assetId] = c, a), {});
+        assetList.forEach((c, i, a, e = h[c.parentId]) => {
+            (e ? (e._children) : r).push(c)
+        });
+        return r;
+    }
 
-  handleCancelAssets = () => {
-    cancelAssets({
-      assetList: this.selectedRows,
-      cancelDate: this.cancelledDate
-    })
-      .then((data) => {
-        console.log(data);
-      })
-      .catch((error) => {
-        this.error = error;
-      });
-    this.isCancelDatePopup = false;
-  };
+    handleRowSelection(event) {
+        this.selectedRows = [];
+        let selectedRows = event.detail.selectedRows;
+        if (selectedRows.length > 0) {
+            let tempList = [];
+            selectedRows.forEach(selectedRecord => {
+                tempList.push(selectedRecord.assetId);
+                this.selectedRows.push(selectedRecord);
+            })
 
-  handleAmendAssets = () => {
-    amendAssets({
-      assetList: this.selectedRows,
-      startDate: this.amendedDate,
-      quantityChange: this.amendedQuantity
-    })
-      .then((data) => {
-        console.log(data);
-      })
-      .catch((error) => {
-        this.error = error;
-      });
-    this.isAmendDatePopup = false;
-  };
+            let assetList = Array.from(this.assetMap.values());
+            assetList.forEach(asset => {
+                if (asset.parentId !== undefined) {
 
-  handleCancelDate(event) {
-    this.cancelledDate = event.currentTarget.value;
-  }
+                    if (tempList.includes(asset.parentId)) {
+                        tempList.push(asset.assetId);
+                        this.selectedRows.push(asset);
+                    }
+                }
+            })
+            this.selectedRowsAPI = tempList;
+        }
 
-  handleAmendDate(event) {
-    this.amendedDate = event.currentTarget.value;
-  }
+        this.isToggle.isButtonsActivated = selectedRows.length > 0 ? false : true;
+    }
 
-  handleQuantity(event) {
-    this.amendedQuantity = event.currentTarget.value;
-  }
+    toggleCancelDatePopup = () => {
+        this.isToggle.isCancelDatePopup = this.isToggle.isCancelDatePopup === false ? true : false
+    }
 
-  get toggleCancelAssetButton() {
-    return this.selectedRows.length > 0 && this.cancelledDate !== undefined
-      ? false
-      : true;
-  }
+    toggleAmendDatePopup = () => {
+        this.isToggle.isAmendDatePopup = this.isToggle.isAmendDatePopup === false ? true : false
+    }
 
-  get toggleAmendAssetButton() {
-    return this.selectedRows.length > 0 && this.amendedDate !== undefined
-      ? false
-      : true;
-  }
+    get toggleCancelAssetButton() {
+        return this.selectedRowsAPI.length > 0 && this.cancelledDate !== undefined ? false : true;
+    }
+
+    get toggleAmendAssetButton() {
+        return this.selectedRowsAPI.length > 0 && this.amendedDate !== undefined && this.quantity !== undefined ? false : true;
+    }
+
+    handleAction = event => {
+        let actionType = event.currentTarget.name;
+        this.isToggle.isLoaded = true;
+        if (actionType === 'Renew') {
+            this.handleRenewAssets();
+        }
+        else if (actionType === 'Cancel') {
+            this.handleCancelAssets();
+        }
+        else if (actionType === 'Amend') {
+            this.handleAmendAssets();
+        }
+        this.isToggle.isLoaded = false;
+        this.isToggle.isCancelDatePopup = false;
+    }
+
+
+    handleRenewAssets = () => {
+        renewAssets({ assetList: this.selectedRows })
+            .then((data) => {
+                this.processAPIRequests(data);
+            })
+            .catch((error) => {
+                this.error = error;
+            });
+    }
+
+    handleCancelAssets = () => {
+        cancelAssets({ assetList: this.selectedRows, cancelDate: this.cancelledDate })
+            .then((data) => {
+                this.processAPIRequests(data);
+            })
+            .catch((error) => {
+                this.error = error;
+            });
+        this.isToggle.isCancelDatePopup = false;
+    }
+
+    handleAmendAssets = () => {
+        amendAssets({ assetList: this.selectedRows, amendDate: this.amendedDate, quantity: this.quantity })
+            .then((data) => {
+                this.processAPIRequests(data);
+            })
+            .catch((error) => {
+                this.error = error;
+            });
+        this.isToggle.isAmendDatePopup = false;
+    }
+
+    processAPIRequests = (data) => {
+        const status = 'Submitted'
+        data.forEach(asset => {
+            let assetRecord = this.assetMap.get(asset.assetId);
+            assetRecord.StatusURL = this.a_Record_URL + '/' + asset.statusURL;
+            assetRecord.Status = status;
+            assetRecord.requestIdentifier = asset.requestIdentifier;
+            this.asyncIdList.add(asset.statusURL);
+        });
+        this.columns = columnsUpdated;
+        this.assetList = this.generateTree(Array.from(this.assetMap.values()));
+    }
+
+    handleDate(event) {
+        this.cancelledDate = event.currentTarget.value;
+    }
+
+    handleAmendDate(event) {
+        let elemId = event.currentTarget.dataset.id
+        if (elemId === 'AmendDate')
+            this.amendedDate = event.currentTarget.value;
+
+        if (elemId === 'quantity')
+            this.quantity = event.currentTarget.value;
+    }
+
+    handleEvent(event) {
+        let obj = event.detail.data.payload;
+        let data = Array.from(this.assetMap.values());
+        data.forEach(asset => {
+            if (asset.requestIdentifier == obj.RequestIdentifier) {
+                this.removeAsyncId(asset.statusURL)
+                if (obj.HasErrors) {
+                    asset.Status = 'Completed With Failures';
+                    asset.StatusURL = this.a_Record_URL + '/lightning/r/RevenueTransactionErrorLog/' + asset.assetId + '/related/PrimaryRevenueTransactionErrorLogs/view';
+                }
+                else {
+                    if (obj.hasOwnProperty('RenewalRecordId')) {
+                        asset.Status = 'Renewed';
+                        asset.StatusURL = this.a_Record_URL + '/' + obj.RenewalRecordId;
+                    }
+                    else if (obj.hasOwnProperty('CancellationRecordId')) {
+                        asset.Status = 'Cancelled';
+                        asset.StatusURL = this.a_Record_URL + '/' + obj.CancellationRecordId;
+                    }
+                    else {
+                        asset.Status = 'Amended';
+                        asset.StatusURL = this.a_Record_URL + '/' + obj.AmendmentRecordId;
+                    }
+                }
+            }
+        })
+        this.assetList = this.generateTree(data);
+    }
+
+    removeAsyncId(statusURL) {
+        this.asyncIdList.delete(statusURL);
+    }
 }
